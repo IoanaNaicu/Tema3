5d885af77e12483bb3397903367823646a090c12
IoanaNaicu <54493390+IoanaNaicu@users.noreply.github.com>
2019-09-04 14:16:26 +0300

    Update TestRobot.java

--- TestRobot.java
+++ TestRobot.java
@@ -1,9 +1,12 @@
 public class TestRobot {
     public static void main(String[] args) {
         Robot robot1 = new Robot();
-        System.out.println("Robot' initial position is: " + robot1.x);
-        robot1.changePos(0);
+        System.out.println("Robot's initial position is: " + robot1.x);
+        robot1.changePos(8);
         robot1.getPos();
+        robot1.changePos(2);
+        robot1.getPos();
+
 
     }
 }
