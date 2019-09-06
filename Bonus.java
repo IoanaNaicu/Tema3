bac2f316feacdbc5113e1f6acbdaf06f3a90cd1a
IoanaNaicu <54493390+IoanaNaicu@users.noreply.github.com>
2019-09-06 09:39:27 +0300

    Add files via upload

--- Bonus.java
+++ Bonus.java
@@ -1,15 +1,25 @@
 public class Bonus {
 
     public void print(String[] mystring1, String[] mystring2, int x) {
-        System.out.println(mystring2[x-1] + ": " + mystring1[x-1]);
+        if (x < 1 || x > 6  ) {
+            System.out.println("Ai introdus o pozitie gresita");
+        } else {
+            System.out.println(mystring2[x - 1] + ": " + mystring1[x - 1]);
+        }
     }
 
-    public void print2(String[] myString1, String[] myString2, String myString, int x){
-        if (myString2[x] == myString) {
-            System.out.println(myString1[x]);
-       }
-        else {
-            System.out.println("nu ai introdus o valoare din contract corecta");
+
+    public void print2(String[] myString1, String[] myString2, String valoare) {
+        for (int i = 0; i < myString2.length; i++) {
+            if (myString2[i].equals(valoare)) {
+                System.out.println(myString1[i]);
+
+            }
+//            else {
+//                System.out.println("Ai introdus o valoare gresita");    // am vrut sa pun un else, ca la o valoare introdusa care nu se regaseste in lista sa afiseze un mesaj (1 singura data)si sa iasa
+//                                                                        // daca pun else-ul aici imi afiseaza "Ai introdus o valoare gresita" de atatea ori cate valori au ramas lui i (de 5 ori)
+//
+//            }
         }
     }
 }
